fb3971ab3275289f4b74e990ff0ae5b968f8003d
sdual <shr.nrht.0623@gmail.com>
2019-10-16 04:41:24 +0900

    remove config file

--- src/main/java/com/github/sdual/funcexecutor/FunctionComposer.java
+++ src/main/java/com/github/sdual/funcexecutor/FunctionComposer.java
@@ -2,10 +2,10 @@ package com.github.sdual.funcexecutor;
 
 import com.github.sdual.funcexecutor.function.ComposableFunction;
 import com.github.sdual.funcexecutor.function.SampleFunction;
+import com.sun.tools.javac.util.Pair;
 import java.lang.reflect.Method;
 import java.util.List;
 import java.util.function.Function;
-import com.sun.tools.javac.util.Pair;
 
 public class FunctionComposer {
 
@@ -13,6 +13,7 @@ public class FunctionComposer {
       throws ClassNotFoundException, NoSuchMethodException {
 
     Pair<String, String> funcName = funcNames.remove(0);
+    // reflectionを使って関数と型名から関数をとってくる
     Method method = SampleFunction.class.getMethod(funcName.fst, Class.forName(funcName.snd));
     if (funcNames.size() == 0) {
       return new ComposableFunction<>(method);
--- src/main/java/com/github/sdual/funcexecutor/Main.java
+++ src/main/java/com/github/sdual/funcexecutor/Main.java
@@ -1,32 +1,24 @@
 package com.github.sdual.funcexecutor;
 
 import com.sun.tools.javac.util.Pair;
-import com.typesafe.config.Config;
-import com.typesafe.config.ConfigFactory;
-import com.typesafe.config.ConfigList;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 
 public class Main {
 
   public static void main(String[] args) throws NoSuchMethodException, ClassNotFoundException {
-    Config conf = ConfigFactory.load();
-    List<Pair<String, String>> functionNameList = extractFuncAndTypeName(conf);
+    // このような関数と引数の名前を設定ファイルからとってきたとする。
+    List<Pair<String, String>> functionNameList = new ArrayList<>();
+    functionNameList.add(Pair.of("addTenString", "java.lang.Integer"));
+    functionNameList.add(Pair.of("transformDouble", "java.lang.String"));
+    functionNameList.add(Pair.of("split", "java.lang.Double"));
 
+    // 設定の上から順番に関数を合成する。
     Function<Integer, List<String>> f2 = FunctionComposer.compose(functionNameList);
 
+    // 合成関数を実行
     System.out.println(f2.apply(10));
-  }
-
-  private static List<Pair<String, String>> extractFuncAndTypeName(Config conf) {
-    ConfigList functionNameConf = conf.getList("functions");
-    List<Pair<String, String>> functionNameList =
-        functionNameConf.unwrapped().stream().map(nameObj -> (HashMap<String, String>) nameObj)
-            .map(name -> Pair.of(name.get("name"), name.get("param_type")))
-            .collect(Collectors.toList());
-    return functionNameList;
   }
 
 }
