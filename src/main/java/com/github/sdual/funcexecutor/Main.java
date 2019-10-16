edf442f18c48f52910e3653f9090ea165475280a
sdual <shr.nrht.0623@gmail.com>
2019-10-16 13:44:54 +0900

    add comment

--- src/main/java/com/github/sdual/funcexecutor/Main.java
+++ src/main/java/com/github/sdual/funcexecutor/Main.java
@@ -1,5 +1,7 @@
 package com.github.sdual.funcexecutor;
 
+import com.github.sdual.funcexecutor.function.ComposableFunction;
+import com.github.sdual.funcexecutor.function.SampleFunction;
 import com.sun.tools.javac.util.Pair;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,17 +10,28 @@ import java.util.function.Function;
 public class Main {
 
   public static void main(String[] args) throws NoSuchMethodException, ClassNotFoundException {
+
+    // 関数の合成
+    Function<Integer, List<String>> f2 =
+        new ComposableFunction<>(SampleFunction::addAndToString,
+            new ComposableFunction<>(SampleFunction::transformToDouble,
+                new ComposableFunction<>(SampleFunction::splitWithDot)));
+
+    System.out.println(f2.apply(10));
+
+
+    // 設定ファイルから関数名をとってきて、動的に関数合成をする。
     // このような関数と引数の型の名前を設定ファイルからとってきたとする。
     List<Pair<String, String>> functionNameList = new ArrayList<>();
     functionNameList.add(Pair.of("addAndToString", "java.lang.Integer"));
     functionNameList.add(Pair.of("transformToDouble", "java.lang.String"));
     functionNameList.add(Pair.of("splitWithDot", "java.lang.Double"));
 
-    // 設定の上から順番に関数を合成する。
-    Function<Integer, List<String>> f2 = FunctionComposer.compose(functionNameList);
-
+    // 設定の上から動的に関数を合成する。
+    Function<Integer, List<String>> f1 = FunctionComposer.compose(functionNameList);
     // 合成関数を実行
-    System.out.println(f2.apply(10));
+    System.out.println(f1.apply(10));
+
   }
 
 }
