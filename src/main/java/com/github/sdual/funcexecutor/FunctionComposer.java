88cd29fe6a001484791dd8a3e6c19b3fe7c6db05
sdual <shr.nrht.0623@gmail.com>
2020-07-24 14:35:10 +0900

    remove main class and add test

--- src/main/java/com/github/sdual/funcexecutor/FunctionComposer.java
+++ src/main/java/com/github/sdual/funcexecutor/FunctionComposer.java
@@ -8,12 +8,16 @@ import java.util.function.Function;
 
 public class FunctionComposer {
 
-  public static <T, R> Function<T, R> compose(List<FunctionName> funcNames)
-      throws ClassNotFoundException, NoSuchMethodException {
+  public static <T, R> Function<T, R> compose(List<FunctionName> funcNames) {
 
     FunctionName funcName = funcNames.remove(0);
     // reflectionを使って関数と型名から関数をとってくる
-    Method method = SampleFunction.class.getMethod(funcName.getName(), Class.forName(funcName.getParamType()));
+    Method method = null;
+    try {
+      method = SampleFunction.class.getMethod(funcName.getName(), Class.forName(funcName.getParamType()));
+    } catch (ClassNotFoundException | NoSuchMethodException e) {
+      throw new RuntimeException(e);
+    }
     if (funcNames.size() == 0) {
       return new ComposableFunction<>(method);
     }
---
+++ src/test/java/com.github.sdual.funcexecutor/FunctionExecutorTest.java
@@ -0,0 +1,46 @@
+package com.github.sdual.funcexecutor;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import com.github.sdual.funcexecutor.function.ComposableFunction;
+import com.github.sdual.funcexecutor.function.SampleFunction;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import org.junit.Test;
+
+public class FunctionExecutorTest {
+
+  @Test
+  public void testExecuteFunctionByConfig() {
+    List<FunctionName> functionNameList = new ArrayList<>();
+    functionNameList.add(FunctionName.of("addAndToString", "java.lang.Integer"));
+    functionNameList.add(FunctionName.of("transformToDouble", "java.lang.String"));
+    functionNameList.add(FunctionName.of("splitWithDot", "java.lang.Double"));
+
+    Function<Integer, List<String>> f1 = FunctionComposer.compose(functionNameList);
+    List<String> actual = f1.apply(10);
+    List<String> expected = new ArrayList<>();
+    expected.add("20");
+    expected.add("0");
+
+    assertThat(actual, is(expected));
+  }
+
+  @Test
+  public void testComposeFunction() {
+    Function<Integer, List<String>> f2 =
+            ComposableFunction.from(SampleFunction::addAndToString,
+                    ComposableFunction.from(SampleFunction::transformToDouble,
+                            ComposableFunction.from(SampleFunction::splitWithDot)));
+
+    List<String> actual = f2.apply(10);
+    List<String> expected = new ArrayList<>();
+    expected.add("20");
+    expected.add("0");
+
+    assertThat(actual, is(expected));
+  }
+
+}
